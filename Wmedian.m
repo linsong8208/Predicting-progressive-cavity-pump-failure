4cbe31e7d28e1b7f9803f772a08dd41b6b8bbb9e
Thiaga <thiagunagu@gmail.com>
2017-04-10 19:24:42 -0500

    Add files via upload

---
+++ Wmedian.m
@@ -0,0 +1,4 @@
+function Y = Wmedian(X)
+% Copyright (c) 2015, MathWorks, Inc.
+    Y = median(X,2);
+end
---
+++ Wrange.m
@@ -0,0 +1,4 @@
+function Y = Wrange(X)
+% Copyright (c) 2015, MathWorks, Inc.
+    Y = range(X,2);
+end
